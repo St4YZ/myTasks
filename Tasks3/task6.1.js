4ad9f962fcd36474e0815b5fe7c4f6f148a81508
St4YZ <hanhur.maksym@gmail.com>
2023-10-15 16:25:37 +0300

    task6.1 adding

---
+++ Tasks3/task6.1.js
@@ -0,0 +1,28 @@
+// *Advanced. Створіть обєкт user з властивостями name, age, email, address.
+// Address має бути обєктом address: {city: "your value", postalCode: "your value"}.
+// Спробуйте створити функції `getUserInfo` та `getFullAddress` в одному рядку за допомогою деструктуризації аргументу функції.
+
+const user = {
+    name: "Maksym",
+    age: "22",
+    email: "hanhur.maksym@gmail.com",
+    address: {
+        city: "Lviv",
+        postalCode: "79038"
+    }
+}
+// ------------------------------------------------------------------------------
+// Функція курильщика!!!
+const getUserInfo = function({name,age}) { console.log("User name is:", name, "\n" + "User age is:",age)}; const getUserAddress = function({city,postalCode}) { console.log("User name is:", city, "\n" + "User age is:",postalCode)};
+getUserInfo(user)
+getUserAddress(user.address)
+// ------------------------------------------------------------------------------
+// Функція здорової людини!!!
+// function getUserInfo ({name,age}){
+//     console.log("User name is:", name, "\n" + "User age is:",age)
+// }
+// getUserInfo(user)
+// function getFullAddress ({city,postalCode}){
+//     console.log("User name is:", city, "\n" + "User age is:",postalCode)
+// }
+// getFullAddress(user.address)
